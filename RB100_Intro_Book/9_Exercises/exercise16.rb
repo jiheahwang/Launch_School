60c140e857994abf2f73caafef135ba30ac5a3fd
Ji Hea Hwang <jiheahwang15@gmail.com>
2023-08-06 02:22:29 +0000

    retried bonus challenge for RB100 9_Exercises exercise16

--- RB100_Intro_Book/9_Exercises/exercise16.rb
+++ RB100_Intro_Book/9_Exercises/exercise16.rb
@@ -1,29 +1,28 @@
-contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
-            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
-
-contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
-fields = [:email, :address, :phone]
-
 # Expected output:
 #  {
 #    "Joe Smith"=>{:email=>"joe@email.com", :address=>"123 Main st.", :phone=>"555-123-4567"},
 #    "Sally Johnson"=>{:email=>"sally@email.com", :address=>"404 Not Found Dr.",  :phone=>"123-234-3454"}
 #  }
 
-contact_details = Hash.new 
+def contact_hash_with_fields(field_array, data)
+  contact_details = {}
+  n = 0
 
-n = 0
-fields.each do |field|
-  contact_details[field] = contact_data.first[n]
-  n+=1
+  field_array.each do |field|
+    contact_details[field] = data[n]
+    n+=1
+  end
+  return contact_details
 end
 
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+fields = [:email, :address, :phone]
 
-contact_names = contacts.keys #array version of contacts (names)
-contact_names.each do |name|
-  contacts[name] = contact_details
-  contact_data.shift #unable to figure out how to get this to work
+contacts.each_key do |key|
+  contacts[key] = contact_hash_with_fields(fields, contact_data.first)
+  contact_data.shift
 end
-  
+
 p contacts
-
