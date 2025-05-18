bbfec5e05bced6032186eb50b07030aec88e438d
jihhwa <jiheahwang15@gmail.com>
2025-05-17 22:36:07 -0400

    add some bonus calculator features

--- Python/PY101/lesson2/calculator/calculator.py
+++ Python/PY101/lesson2/calculator/calculator.py
@@ -1,48 +1,63 @@
+import os
+import operator
+
+OPERATORS = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+    "/": operator.truediv,
+}
+
 def prompt(message):
     print(f'==> {message}')
 
-def invalid_number(number_str):
-    try:
-        float(number_str)
-    except ValueError:
-        return True
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-    return False
+def get_valid_number(message):
+    while True:
+        prompt(message)
+        try:
+            number = float(input().strip())
+        except ValueError:
+            prompt("Invalid input. Please enter a valid number.\n")
+        else:
+            return number
 
+def perform_one_calculation():
+    number1 = get_valid_number("What's the first number?")
+    number2 = get_valid_number("What's the second number?")
 
+    while True:
+        prompt('What operation would you like to perform?\n'
+            'Enter +, -, *, or /')
+        operation = input().strip()
+
+        # check for zero division
+        if operation == '/' and number2 == 0.0:
+            prompt('Cannot divide by 0. Please choose another operation.\n')
+            continue
+
+        if operation in OPERATORS:
+            output = OPERATORS[operation](number1, number2)
+            break # break out of this loop if everything is good
+
+        prompt('You must choose +, -, *, or /\n')
+
+    # format specifier g removes unnecessary decimal points
+    prompt(f'{number1:g} {operation} {number2:g} is {output:g}\n')
+
+# main
+clear_screen()
 prompt('Welcome to Calculator!')
 
-prompt("What's the first number?")
-number_str1 = input()
+while True:
+    perform_one_calculation()
+    prompt('Would you like to perform another calculation? y/n')
+    answer = input().strip()
+    if answer.lower().startswith('y'):
+        clear_screen()
+    else:
+        break
 
-while invalid_number(number_str1):
-    prompt("Hmm... that doesn't look like a valid number.")
-    number_str1 = input()
-
-prompt("What's the second number?")
-number_str2 = input()
-
-while invalid_number(number_str2):
-    prompt("Hmm... that doesn't look like a valid number.")
-    number_str2 = input()
-
-prompt('What operation would you like to perform?\n'
-      '1) Add 2) Subtract 3) Multiply 4) Divide')
-operation = input()
-
-while operation not in ["1", "2", "3", "4"]:
-    prompt('You must choose 1, 2, 3, or 4')
-    operation = input()
-
-# match/case for operation
-match operation:
-    case "1":
-        output = float(number_str1) + float(number_str2)
-    case "2":
-        output = float(number_str1) - float(number_str2)
-    case "3":
-        output = float(number_str1) * float(number_str2)
-    case "4":
-        output = float(number_str1) / float(number_str2)
-
-prompt(f'The result is: {output}')
+prompt('Thanks for using the calculator. Goodbye!')
