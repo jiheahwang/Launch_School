627ee5d6adeda3e1e816524066c3ac2af5c9ac11
jihhwa <jiheahwang15@gmail.com>
2025-05-19 16:16:59 -0400

    loan amount validation done

--- Python/PY101/lesson2/mortgage_calculator.py
+++ Python/PY101/lesson2/mortgage_calculator.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 def prompt(message):
     print(f'==> {message}')
@@ -6,10 +7,39 @@ def prompt(message):
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-#TODO: validation - 2 decimal places; only positive numbers (can't be 0 or lower); handle comma and $ (REGEX opportunity!)
 def get_loan_amount() -> float:
-    prompt('What is the loan amount? ex) $50,000.00 | 50000 | 50,000.55')
-    return 50000.00
+    def is_valid_money_format(user_input):
+        pattern = re.compile(r"""
+        \$?                         # allow a single optional $ sign
+        (
+            (                       # cases 1 and 2 inside this parenthesis
+                (                   # case 1 - for comma separated inputs:
+                    [1-9]           # check that the number doesn't have leading 0s
+                    \d{0,2}         # possible remaining 0-2 digits before the...
+                    (,\d{3})*       # possible comma separated groups of 3 digits
+                    |[1-9]\d*       # or case 2: plain numbers without commas
+                )
+                (\.\d{0,2})?        # then optional decimal point with 0-2 digits
+            )|                      # or:
+            (0\.(0[1-9]|[1-9]\d))   # case 3 - allow 0.01-0.99
+        )
+        """, re.VERBOSE)
+
+        return pattern.fullmatch(user_input)
+
+    while True:
+        prompt('What is the loan amount? ex) $50,000.00 | 50000 | 50,000.55')
+        user_input = input().strip()
+
+        if is_valid_money_format(user_input):
+            loan_amount = float(user_input.replace(',','').replace('$',''))
+            if loan_amount < 1:
+                prompt("I hope you didn't take out a loan for mere cents!")
+            return loan_amount
+        else:
+            prompt('Invalid input. Please enter a valid amount.\n' \
+            '\tvalid  : $50,000.00 | 50000 | 50,000.55 | $0.99\n' \
+            '\tinvalid: $0 | -5000 | $040 | 40,00,00 | $.99')
 
 #TODO: validation - handle percent sign
 def get_apr() -> float:
@@ -31,15 +61,16 @@ def run_mortgage_calculator_once():
     # collect info from user
     loan_amount = get_loan_amount()
     apr_percent = get_apr()
-    monthly_rate = apr_percent / 100 / 12
     loan_months = get_loan_duration_months()
 
     # do calculations
+    monthly_rate = apr_percent / 100 / 12
     monthly_payment = calculate_monthly_payment(monthly_rate, loan_amount, loan_months)
     total_amount = monthly_payment * loan_months
     total_interest = total_amount - loan_amount
 
     # output result
+    clear_screen()
     prompt(f'''For a ${loan_amount:,.2f}, {apr_percent}% APR, {loan_months} month loan:
         Monthly payment     : ${monthly_payment:,.2f}
         Number of payments  : {loan_months}
@@ -72,4 +103,4 @@ while True:
 
 prompt('Thank you for using the Mortgage Calculator. Goodbye!')
 
-# Finally, don't forget to run your code through Pylint.
+# TODO: pylint
